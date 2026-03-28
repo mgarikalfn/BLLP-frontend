164f4e5b70a706eb5ccc25430ebaaef6e1cac2c6
Edenawit21 <edidinkayehudelesa@gmail.com>
2026-03-28 15:58:58 -0700

    add topic image

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -37,7 +37,7 @@ export default function  Sidebar  ({ className }: Props)  {
          <SidebarItem
           label="topic"
           href="/topic"
-          icon={BookOpen}
+          iconSrc="/topic.jpg"
         />
        
         {/* <SidebarItem
